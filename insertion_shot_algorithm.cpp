4190b83c7894661603c4645ccb657d2139b48eeb
Pratap <suryapratapsing8@gmail.com>
2022-06-20 13:13:59 +0530

    "insertion shot and linear search"

---
+++ insertion_shot_algorithm.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void insertionshort(int arr[],int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
+int main(){
+    int n;
+    cin>>n;   
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    insertionshort(arr,n);
+    return 0;
+}
